07a0992a1d20f83dbabb89e717a64669d5d929f7
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-02-04 22:01:13 -0200

    Check network connectivity before trigger request

--- EyeOnBuzz/Support/NotificationNames.swift
+++ EyeOnBuzz/Support/NotificationNames.swift
@@ -10,4 +10,5 @@ import Foundation
 
 extension Notification.Name {
     static let invalidAPIKey = Notification.Name("InvalidAPIKey")
+    static let unreachableNetwork = Notification.Name("UnreachableNetwork")
 }
